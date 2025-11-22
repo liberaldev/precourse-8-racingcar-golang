b68a57bff4784b654bca4eaf0c9787f855b4290d
liberaldev <ssharp0614@gmail.com>
2025-11-22 17:51:13 +0900

    feat: 일단 자동차 이름을 인자로 받는 것을 구현

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	args := os.Args[1:] // 첫 번째 인자(프로그램 이름) 제외
+
+	var carNames []string
+	var times int
+
+	// --times 플래그 찾기
+	for i := 0; i < len(args); i++ {
+		if args[i] == "--times" && i+1 < len(args) {
+			if t, err := strconv.Atoi(args[i+1]); err == nil {
+				times = t
+			}
+			break
+		}
+		carNames = append(carNames, args[i])
+	}
+
+	fmt.Printf("자동차 이름: %v\n", carNames)
+	fmt.Printf("시도 횟수: %d\n", times)
 }
