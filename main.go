cae0c437dab7b5b4593ec861693ce4e1b1e0a98f
liberaldev <ssharp0614@gmail.com>
2025-11-22 18:38:04 +0900

    feat: times flag 예외 처리 추가

--- main.go
+++ main.go
@@ -10,13 +10,21 @@ func main() {
 	args := os.Args[1:] // 첫 번째 인자(프로그램 이름) 제외
 
 	var carNames []string
-	var times int
+	times := -1
 
 	// --times 플래그 찾기
 	for i := 0; i < len(args); i++ {
-		if args[i] == "--times" && i+1 < len(args) {
-			if t, err := strconv.Atoi(args[i+1]); err == nil {
-				times = t
+		if args[i] == "--times" {
+			if i+1 < len(args) {
+				if t, err := strconv.Atoi(args[i+1]); err == nil && t > 0 {
+					times = t
+				} else if err != nil {
+					fmt.Println("올바른 값을 입력하지 않았습니다")
+					os.Exit(1)
+				} else if t < 0 {
+					fmt.Println("1보다 큰 정수를 입력하세요")
+					os.Exit(1)
+				}
 			}
 			break
 		}
