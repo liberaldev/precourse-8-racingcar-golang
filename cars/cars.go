f483a981c06b536e7893148519deed77eace0580
liberaldev <ssharp0614@gmail.com>
2025-11-23 15:06:43 +0900

    feat: 각 배열을 순회할때 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상의 경우 `cars`의 요소인 `Car` 구조체의 ```steps` 필드의 수를 증가하는 메소드 구현

--- cars/cars.go
+++ cars/cars.go
@@ -1,5 +1,10 @@
 package cars
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 type Car struct {
 	name  string
 	steps int
@@ -12,5 +17,13 @@ type Cars struct {
 func (c *Cars) Init(names []string) {
 	for i := 0; i < len(names); i++ {
 		c.cars = append(c.cars, Car{name: names[i], steps: 0})
+	}
+}
+
+func (c *Cars) MoveCarsByRandomNumber() {
+	for _, car := range c.cars {
+		if n, err := rand.Int(rand.Reader, big.NewInt(9)); err == nil && n.Int64() >= 4 {
+			car.steps += 1
+		}
 	}
 }
