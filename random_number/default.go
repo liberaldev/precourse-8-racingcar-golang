61122041dd7a6208a60cddf6c84af1f79de228bd
liberaldev <ssharp0614@gmail.com>
2025-11-24 19:27:38 +0900

    refactor: 기본 랜덤 함수가 에러를 제대로 처리하도록 수정

--- random_number/default.go
+++ random_number/default.go
@@ -9,5 +9,8 @@ type DefaultRandomGenerator struct{}
 
 func (d *DefaultRandomGenerator) GetRandomNumber(maxNum *big.Int) (int64, error) {
 	n, err := rand.Int(rand.Reader, maxNum)
+	if err != nil {
+		return -1, err
+	}
 	return n.Int64(), err
 }
