25af99f39b4cd607293b22bfffe836eae6734f79
liberaldev <ssharp0614@gmail.com>
2025-11-24 20:14:02 +0900

    fix: 오해의 소지가 있는 오류 문구 수정

--- random_number/mock.go
+++ random_number/mock.go
@@ -17,7 +17,7 @@ func InitMockRandomGenerator(numbers []int64) *MockRandomGenerator {
 func (m *MockRandomGenerator) GetRandomNumber(maxNum *big.Int) (int64, error) {
 	num := m.numbers[m.index]
 	if num > maxNum.Int64() {
-		return -1, errors.New("maxNum 인자보다 큰 수를 입력하셨습니다")
+		return -1, errors.New("maxNum 인자보다 큰 수가 numbers 필드에 들어가 있습니다")
 	}
 	m.index++
 	return num, nil
